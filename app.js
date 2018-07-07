6ecb292fdc877287be016163799ee4186fa2c2b8
Your Name <nazik_111@mail.ru>
2018-07-07 12:44:23 +0500

    add function to highlight repeated words

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ var wordsBank = [];
 var wordsCount = 0;
 
 var boxes = document.querySelectorAll('.square');
+var tbody = document.querySelector('.t-body');
 //console.log(boxes);
 
 boxes.forEach(function(box) {
@@ -77,6 +78,20 @@ function createList(word, points, number) {
   wordsList.innerHTML = tr;
 }
 
+function highlightWord(target_word) {
+  var tbody = document.querySelector('.t-body');
+  var children = tbody.children;
+  for (var i = 0; i <= children.length; i++) {
+    let child = children[i].children[1];
+    let text = child.innerHTML;
+    if (text === target_word) {
+      console.log('repeated');
+      child.classList.add('has-text-warning');
+    }
+    console.log(child);
+  }
+}
+
 function loadData() {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'words_dictionary_full.json', true);
@@ -86,22 +101,28 @@ function loadData() {
       //console.log(words);
 
       // check if there is a word in the database
-      if (binarySearch(words, searched_word.toLocaleLowerCase()) !== -1) {
+      if (
+        binarySearch(words, searched_word.toLocaleLowerCase()) !== -1 &&
+        !checkRepeatedWords(wordsBank, searched_word)
+      ) {
         let wordsPoint = searched_word.length;
-        if (!checkRepeatedWords(wordsBank, searched_word)) {
-          console.log('uppps');
-          createList(searched_word, wordsPoint, wordsCount);
-          wordsBank.push(searched_word);
-          wordsCount += 1;
-        }
+
+        createList(searched_word, wordsPoint, wordsCount);
+        wordsBank.push(searched_word);
+        wordsCount += 1;
+        searched_word = '';
+        document.querySelector('.input').value = '';
         //console.log(words);
 
         //console.log(wordsBank);
         //console.log('done');
-      }
+      } else {
+        console.log('uppps');
+        highlightWord(searched_word);
 
-      searched_word = '';
-      document.querySelector('.input').value = '';
+        searched_word = '';
+        document.querySelector('.input').value = '';
+      }
     }
   };
   xhr.send();
